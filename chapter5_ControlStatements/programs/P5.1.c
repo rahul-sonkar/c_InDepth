19edcfce577123959eb9f693ade78605a96c2e79
Rahul Sonkar (wsl) <rahulsonkar28398@gmail.com>
2022-10-08 22:04:44 +0530

    Print the big numder given by user

--- chapter5_ControlStatements/programs/P5.1.c
+++ chapter5_ControlStatements/programs/P5.1.c
@@ -3,9 +3,10 @@
 int main()
 {
     int a, b;
+    printf("Enter two numbers : ");
     scanf("%d %d", &a, &b);
     if (a > b)
-        printf("a is greater..\n");
+        printf("%d is greater..\n",a);
     else
-        printf("b is greater..\n");
+        printf("%d is greater..\n",b);
 }
